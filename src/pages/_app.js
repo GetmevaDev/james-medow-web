21c26edab0fe677e04507c7c85a1e3da18855c5e
GetmevaDev <devteam@getmeva.com>
2023-06-23 14:22:59 +0600

    fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,18 @@
+import { Montserrat } from "@next/font/google";
+
+import "@/styles/globals.scss";
+
+export const mont = Montserrat({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${mont.variable} `}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
