4abbca71b6994f7f3e29b73634b09ccf3135ccfb
GetmevaDev <devteam@getmeva.com>
2023-06-28 22:51:29 +0600

    add gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import { Montserrat } from "@next/font/google";
 
 import "swiper/css";
@@ -15,6 +17,26 @@ export const mont = Montserrat({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${mont.variable} `}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+                 (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-NNNJX7H");
+          `}
+      </Script>
+
+      <Script
+        type="text/javascript"
+        src="//cdn.callrail.com/companies/658899511/3048a1676ebba4a3220e/12/swap.js"
+      />
+
       <Component {...pageProps} />
     </main>
   );
