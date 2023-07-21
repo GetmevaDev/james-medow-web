f9c878d49a60f56f91952055e73ff99f92a8741d
GetmevaDev <devteam@getmeva.com>
2023-07-21 17:00:59 +0600

    video

--- src/components/ui/Video/Video.jsx
+++ src/components/ui/Video/Video.jsx
@@ -1,16 +1,33 @@
 import React, { useRef } from "react";
 
+import { useMediaQuery } from "@/components/hooks";
+
 import styles from "./Video.module.scss";
 
-export const VideoBackground = () => (
-  <div className={styles.video_background}>
-    <video
-      muted
-      autoPlay
-      playsInline
-      loop
-      className={styles.video}
-      src="/video/bg.mov"
-    />
-  </div>
-);
+export const VideoBackground = () => {
+  const isMatches = useMediaQuery("(max-width: 480px)");
+
+  return (
+    <div className={styles.video_background}>
+      {!isMatches ? (
+        <video
+          muted
+          autoPlay
+          playsInline
+          loop
+          className={styles.video}
+          src="/video/bg.mov"
+        />
+      ) : (
+        <video
+          muted
+          autoPlay
+          playsInline
+          loop
+          className={styles.video}
+          src="/video/mobile-test2.mp4"
+        />
+      )}
+    </div>
+  );
+};
