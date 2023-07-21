94d010d8090071190d86ea2e0ede6e3110675972
GetmevaDev <devteam@getmeva.com>
2023-07-21 16:07:43 +0600

    video

--- src/components/ui/Video/Video.jsx
+++ src/components/ui/Video/Video.jsx
@@ -2,26 +2,15 @@ import React, { useRef } from "react";
 
 import styles from "./Video.module.scss";
 
-export const VideoBackground = () => {
-  const videoRef = useRef(null);
-
-  const handleVideoEnd = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime = 0;
-      videoRef.current.play();
-    }
-  };
-
-  return (
-    <div className={styles.video_background}>
-      <video
-        muted
-        autoPlay
-        className={styles.video}
-        src="/video/bg.mov"
-        onEnded={handleVideoEnd}
-        ref={videoRef}
-      />
-    </div>
-  );
-};
+export const VideoBackground = () => (
+  <div className={styles.video_background}>
+    <video
+      muted
+      autoPlay
+      playsInline
+      loop
+      className={styles.video}
+      src="/video/bg.mov"
+    />
+  </div>
+);
