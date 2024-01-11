f8827c4e033ccf705747e591716998ac69bb05c9
GetmevaDev <devteam@getmeva.com>
2024-01-11 19:10:16 +0600

    fix menu

--- src/components/ui/Navigation/NavigationTest.jsx
+++ src/components/ui/Navigation/NavigationTest.jsx
@@ -1,130 +1,182 @@
 import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useLayoutEffect, useRef, useState } from "react";
-import { FaChevronCircleLeft, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  FaChevronCircleLeft,
+  FaChevronLeft,
+  FaChevronRight,
+} from "react-icons/fa";
 
 import { useSize } from "@/components/hooks/useResizeObserver";
+import { truncateText } from "@/components/utils/truncateText";
 
 import { Button } from "..";
 
 import styles from "./NavigationTest.module.scss";
 
 export const NavigationTest = ({ menus }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const [submenuVisibility, setSubmenuVisibility] = useState(true);
-  const router = useRouter();
+  const [activeSubMenu, setActiveSubMenu] = useState(null);
 
-  const toggleSubmenu = (id) => {
-    if (submenuVisibility === id) {
-      setSubmenuVisibility(null);
-    } else {
-      setSubmenuVisibility(id);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = (id) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setIsHovered(id);
+  };
+
+  useEffect(
+    () => () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    },
+    []
+  );
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsHovered(false);
+    }, 500);
+  };
+
+  const handleSubMenuEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
     }
   };
 
-  const renderSubMenuItems = (items, id) => (
-    submenuVisibility === id && (
-      <div className={styles.sub_menu_items}>
-        {items?.map((item) => (
-          <Link
-            href={item.path}
-            key={item.id}
-            className={
-              router.pathname === item.path ? styles.subMenu_item_active : styles.subMenu_item_link
-            }
-          >
-            {item.itemText}
-          </Link>
-        ))}
-      </div>
-    )
+  const handleSubMenuLeave = () => {
+    setIsHovered(false);
+  };
+
+  const router = useRouter();
+
+  const renderSubMenuItems = (items) => (
+    <div className={styles.sub_menu_items}>
+      {items?.map((item) => (
+        <Link
+          href={item.path}
+          key={item.id}
+          className={
+            router.pathname === item.path
+              ? styles.subMenu_item_active
+              : styles.subMenu_item_link
+          }
+        >
+          {truncateText(item.itemText, 50)}
+        </Link>
+      ))}
+    </div>
   );
 
   const renderSubMenu = (subMenu) => (
     <div className={styles.sub_menu}>
-      <div
-        className={styles.sub_menu_inner}
-      >
+      <div className={styles.sub_menu_inner}>
         <div className={styles.left_block_inner}>
           {subMenu?.map((menuItem) => (
             <div key={menuItem.id} className={styles.block}>
-              <div
-                className={styles.left_block}
-              >
-                <Link
-                  className={
-                  classNames(
-                    router.pathname === menuItem.path ? styles.active : styles.link,
-                    menuItem.icon && styles.icon_subMenu
-                  )
-                }
-                  href={menuItem?.path}
-                >
-                  {menuItem?.label}
-                </Link>
+              <div className={styles.left_block}>
+                <div className={styles.icon_inner}>
+                  <Link
+                    onMouseEnter={() => handleMouseEnter(menuItem.id)}
+                    onMouseLeave={handleMouseLeave}
+                    className={classNames(
+                      router.pathname === menuItem.path
+                        ? styles.active
+                        : styles.link,
+                      menuItem.icon && styles.icon_subMenu,
+                      isHovered === menuItem.id && styles.left_block_hovered
+                    )}
+                    href={menuItem?.path}
+                  >
+                    {menuItem?.label}
+                  </Link>
 
-                {menuItem.icon && (
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className={styles.button_icon}
-                  onClick={() => toggleSubmenu(menuItem.id)}
-                >
-                  {submenuVisibility === menuItem.id ? <FaChevronLeft /> : <FaChevronRight />}
+                  {menuItem.icon && (
+                    <div
+                      role="button"
+                      tabIndex={0}
+                      className={classNames(
+                        styles.button_icon,
+                        submenuVisibility === menuItem.id && styles.icon_rotated // Apply rotation if this submenu is visible
+                      )}
+                    >
+                      {isHovered === menuItem.id ? (
+                        <FaChevronLeft />
+                      ) : (
+                        <FaChevronRight />
+                      )}
+                    </div>
+                  )}
                 </div>
-              )}
               </div>
-
             </div>
-        ))}
+          ))}
         </div>
 
         <div>
           {subMenu?.map((menuItem) => (
-            <div>
-              {submenuVisibility && menuItem?.SubMenuItems.length > 0 && (
-              <div
-                className={`${styles.right_block} ${submenuVisibility === menuItem.id ? styles.open : ""}`}
-              >
-                {renderSubMenuItems(menuItem?.SubMenuItems, menuItem.id)}
-              </div>
-          )}
+            <div
+              className={styles.right_block_inner}
+              onMouseEnter={handleSubMenuEnter}
+              onMouseLeave={handleSubMenuLeave}
+            >
+              {isHovered === menuItem.id && (
+                <div className={styles.right_block}>
+                  {renderSubMenuItems(menuItem?.SubMenuItems)}
+                </div>
+              )}
             </div>
-        ))}
+          ))}
         </div>
-
       </div>
     </div>
   );
+
+   const handleMouseEnterSub = (index) => {
+    setActiveSubMenu(index);
+  };
+
+  const handleMouseLeaveSub = () => {
+    // Задержка перед скрытием подменю, чтобы пользователь мог переместить курсор на подменю
+    setTimeout(() => {
+      setActiveSubMenu(null);
+    }, 500); // Задержка в 500 мс
+  };
 
   return (
     <div className={styles.navigation}>
       <nav className={styles.nav}>
         <ul className={styles.list}>
-          {menus?.sort((a, b) => a.id - b.id)?.map((item) => (
-            <li key={item.id}>
-              <Link
-                href={item?.attributes?.path}
-                onClick={() => handleClick(item?.attributes)}
-                className={
-                    classNames(
-                      router.pathname === item?.attributes?.path ? styles.active : styles.link,
-                      item?.attributes?.icon && styles.icon
-                    )
-                }
-              >
-                {item?.attributes?.label}
-              </Link>
-              {item?.attributes?.SubMenu?.length > 0 && renderSubMenu(item?.attributes?.SubMenu)}
-            </li>
-          ))}
+          {menus
+            ?.sort((a, b) => a.id - b.id)
+            .map((item) => (
+              <li key={item.id}>
+                <Link
+                  href={item?.attributes?.path}
+                  className={classNames(
+                    router.pathname === item?.attributes?.path
+                      ? styles.active
+                      : styles.link,
+                    item?.attributes?.icon && styles.icon
+                  )}
+                >
+                  {item?.attributes?.label}
+                </Link>
+                {item?.attributes?.SubMenu?.length > 0 &&
+                  renderSubMenu(item?.attributes?.SubMenu)}
+              </li>
+            ))}
           <a href="tel: 929-205-4935">
             <Button variant="secondary" className={styles.button}>
               Click to call
             </Button>
           </a>
         </ul>
-
       </nav>
     </div>
   );
