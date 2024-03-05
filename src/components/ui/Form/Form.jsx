cf4523d64b74cf08fef31c20caa63746b7aedf7c
Shakhboz Farkhodov <shaxbozfarhodov@gmail.com>
2024-03-05 15:05:26 +0500

    return air ai form

--- src/components/ui/Form/Form.jsx
+++ src/components/ui/Form/Form.jsx
@@ -41,7 +41,7 @@ export const Form = ({ htmlSubCall }) => {
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6Im9yZ18yVXdSMDNzbUplU2FQRGJKRHl4Rmp1UG1FQ1kiLCJpYXQiOjE3MDE5MjY3NDd9.N7xeB-fndbZiL22ZuxrV9CFVFgl_vrngPQ3DGGZ9uuI",
         },
         body: JSON.stringify({
-          promptId: 29212,
+          promptId: 72271,
           metaData: {},
           phone: phoneNumber,
           name,
@@ -83,7 +83,7 @@ export const Form = ({ htmlSubCall }) => {
 
   return (
     <FadeIn>
-      {/* <div className={styles.contact}>
+      <div className={styles.contact}>
         <form onSubmit={handleSubmit} className={styles.form}>
           <div
             className={styles.call}
@@ -147,7 +147,7 @@ export const Form = ({ htmlSubCall }) => {
             </button>
           </div>
         </form>
-      </div> */}
+      </div>
     </FadeIn>
   );
 };
