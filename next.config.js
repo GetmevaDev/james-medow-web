ec020b1740de8eeb65245492fb3122cf26251baf
GetmevaDev <devteam@getmeva.com>
2024-04-12 12:44:58 +0700

    minify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const { default: classnamesMinifier } = require("@nimpl/classnames-minifier");
+
 const nextConfig = {
   reactStrictMode: true,
+
   profiler: true,
   images: {
     domains: ["localhost", "res.cloudinary.com"],
@@ -17,4 +21,13 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = (phase) =>
+  withBundleAnalyzer(
+    classnamesMinifier({
+      prefix: "_",
+      reservedNames: ["_en", "_de"],
+      disabled:
+        phase === PHASE_PRODUCTION_SERVER ||
+        process.env.NODE_ENV === "development",
+    })(nextConfig)
+  );
